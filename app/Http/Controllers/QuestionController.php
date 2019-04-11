8149707256813a84d5d58c9382cd9662a002d7fb
NishaRavichandran <runisha93@gmail.com>
2019-04-11 17:20:12 -0400

    Included Voter Table and made changes in the question controller by adding voting options

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use App\Question;
 use Illuminate\Support\Facades\Auth;
@@ -12,6 +13,7 @@ class QuestionController extends Controller
     {
         $this->middleware('auth');
     }
+
     /**
      * Display a listing of the resource.
      *
@@ -32,14 +34,14 @@ class QuestionController extends Controller
 
         $question = new Question;
         $edit = FALSE;
-        return view('questionForm', ['question' => $question,'edit' => $edit  ]);
+        return view('questionForm', ['question' => $question, 'edit' => $edit]);
 
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -61,7 +63,6 @@ class QuestionController extends Controller
         return redirect()->route('home')->with('message', 'IT WORKS!');
 
 
-
         // return redirect()->route('questions.show', ['id' => $question->id]);
 
     }
@@ -70,7 +71,7 @@ class QuestionController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show(Question $question)
@@ -81,21 +82,21 @@ class QuestionController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
 
     public function edit(Question $question)
     {
         $edit = TRUE;
-        return view('questionForm', ['question' => $question, 'edit' => $edit ]);
+        return view('questionForm', ['question' => $question, 'edit' => $edit]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Question $question)
@@ -113,19 +114,106 @@ class QuestionController extends Controller
         $question->body = $request->body;
         $question->save();
 
-        return redirect()->route('question.show',['question_id' => $question->id])->with('message', 'Saved');
+        return redirect()->route('question.show', ['question_id' => $question->id])->with('message', 'Saved');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy(Question $question)
     {
         $question->delete();
         return redirect()->route('home')->with('message', 'Deleted');
+
+    }
+
+    public function vote(Request $request)
+    {
+        $userId = $request['userId'];
+        $questionsId = $request['questionsId'];
+        $voteType = $request['voteType'];
+        $token = $request['_token'];
+
+        // query below returns vote_type('up' or 'down') or null
+        $vote = DB::table('votes')->where([
+            ['user_id', "=", $request['userId']],
+            ['questions_id', "=", $request['questionsId']]
+        ])->value('vote_type');
+
+        // if user didn't vote
+        if (!$vote) {
+            DB::table('votes')->insert([
+                'questions_id' => $questionsId,
+                'user_id' => $userId,
+                'vote_type' => $voteType
+            ]);
+            $questions = Question::find($questionsId);
+            if ($voteType == "up") {
+                $questionsVotesUp = $questions->votes_up;
+                $questionsVotesUp = $questionsVotesUp + 1;
+                $questions->votes_up = $questionsVotesUp;
+                $questionsVotesDown = $questions->votes_down;
+                $result = $questionsVotesUp - $questionsVotesDown;
+                $questions->result = $result;
+                $questions->update();
+            } else {
+                $questionsVotesDown = $questions->votes_down;
+                $questionsVotesDown = $questionsVotesDown + 1;
+                $questions->votes_down = $questionsVotesDown;
+                $questionsVotesUp = $questions->votes_up;
+                $result = $questionsVotesUp - $questionsVotesDown;
+                $questions->result = $result;
+                $questions->update();
+            }
+            $result = $questions->votes_up - $questions->votes_down;
+            $up = $questions->votes_up;
+            $down = $questions->votes_down;
+            return response()->json(['result' => $result, 'up' => $up, 'down' => $down], 200);
+        }
+        // if user has already voted
+        if ($vote == $voteType) {
+            return response()->json(['warning' => 'You can not vote twice(up or down) for same question'], 200);
+        } else {
+            $questions = Question::find($questionsId);
+            if ($voteType == 'up') {
+                // update votes field in posts Table
+                $questionsVotesDown = $questions->votes_down;
+                $questionsVotesDown = $questionsVotesDown - 1;
+                $questions->votes_down = $questionsVotesDown;
+                $questionsVotesUp = $questions->votes_up;
+                $questionsVotesUp = $questionsVotesUp + 1;
+                $questions->votes_up = $questionsVotesUp;
+                $result = $questionsVotesUp - $questionsVotesDown;
+                $questions->result = $result;
+                $questions->update();
+                DB::table('votes')->where([
+                    ['user_id', "=", $request['userId']],
+                    ['questions_id', "=", $request['questionsId']]
+                ])->update(['vote_type' => $voteType]);
+            } else {
+                // update votes field in posts Table
+                $questionsVotesUp = $questions->votes_up;
+                $questionsVotesUp = $questionsVotesUp - 1;
+                $questions->votes_up = $questionsVotesUp;
+                $questionsVotesDown = $questions->votes_down;
+                $postVotesDown = $questionsVotesDown + 1;
+                $questions->votes_down = $postVotesDown;
+                $result = $questionsVotesUp - $questionsVotesDown;
+                $questions->result = $result;
+                $questions->update();
+                DB::table('votes')->where([
+                    ['user_id', "=", $request['userId']],
+                    ['questions_id', "=", $request['questionsId']]
+                ])->update(['vote_type' => $voteType]);
+            }
+            $result = $questions->votes_up - $questions->vote_down;
+            $up = $questions->votes_up;
+            $down = $questions->votes_down;
+            return response()->json(['result' => $result, 'up' => $up, 'down' => $down], 200);
+        }
 
     }
 }
